48e18e155c5a4f64cb52a7eaec80a5fb875f74d7
Bibi <bibi6666667@kakao.com>
2023-01-19 15:21:18 +0900

    initial commit

---
+++ AutoLayoutPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayoutPractice
+//
+//  Created by Bibi on 2023/01/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
