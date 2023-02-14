890b0dc0b7d8ffd5a525ef04e336132f30fcd50b
Bibi <bibi6666667@kakao.com>
2023-02-14 17:58:50 +0900

    Basic 21. AutoLayout Code Anchor (1)

--- AutoLayoutPractice/ViewController.swift
+++ AutoLayoutPractice/ViewController.swift
@@ -12,6 +12,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let myButton = UIButton.init(type: .system)
+        myButton.setTitle("My Button", for: .normal)
+        
+        self.view.addSubview(myButton)
+        
+        myButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        myButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 100).isActive = true
+        myButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
     }
 
 
