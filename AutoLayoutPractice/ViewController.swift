ccdccb0dcbbd6876c1d494e3fcc6e0dca7d9e1ce
Bibi <bibi6666667@kakao.com>
2023-03-06 18:06:31 +0900

    Expand Cell 2

--- AutoLayoutPractice/ViewController.swift
+++ AutoLayoutPractice/ViewController.swift
@@ -15,19 +15,54 @@ class ExpandCell: UITableViewCell {
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    struct ExpandDataModel {
+        var description: String
+        var isExpand: Bool
+    }
+    
+    var dataModels = [ExpandDataModel]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let textArray = ["short text",
+                         "longlonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglong text",
+                         "short text",
+                         "longlonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglong text",
+                         "short text",
+                         "longlonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglong text"]
+        
+        // 위 텍스트를 데이터 모델에 넣어야 함
+        for (_, value) in textArray.enumerated() {
+            // 셀의 기본값은 확장되어 있지 않음(false)
+            dataModels.append(ExpandDataModel.init(description: value, isExpand: false))
+        }
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return dataModels.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "expandCell_ID", for: indexPath) as! ExpandCell
-        cell.descriptionLabel.text = "aaa"
+        cell.descriptionLabel.text = dataModels[indexPath.row].description
+        
+        if dataModels[indexPath.row].isExpand == true {
+            // 확장했을 때는 텍스트 길이만큼 늘어나도록 함
+            cell.descriptionLabel.numberOfLines = 0
+        } else {
+            // 확장하지 않았을 때는 기본 길이만 보여줌 (1줄)
+            cell.descriptionLabel.numberOfLines = 1
+        }
+        
+        cell.selectionStyle = .none
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        dataModels[indexPath.row].isExpand = !dataModels[indexPath.row].isExpand
+        
+        tableView.reloadRows(at: [indexPath], with: .none)
     }
 }
 
